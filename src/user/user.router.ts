db4a625c0e9daf4dfa4982633128924c4a949229
matias1364 <matiasleoncontreras@gmail.com>
2023-09-25 20:37:55 -0300

    fix router

--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -7,9 +7,9 @@ export class UserRouter extends BaseRouter<UserController>{
     }
     routes(): void {
         this.router.get("/users",(req,res)=>this.controller.getUsers(req,res))
-        this.router.get("/user/:id",(req,res)=>this.controller.getUsers(req,res))
-        this.router.get("/createUser",(req,res)=>this.controller.getUsers(req,res))
-        this.router.get("/updateUser/:id",(req,res)=>this.controller.getUsers(req,res))
-        this.router.get("/deleteUser/:id",(req,res)=>this.controller.getUsers(req,res))
+        this.router.get("/user/:id",(req,res)=>this.controller.getUserById(req,res))
+        this.router.post("/createUser",(req,res)=>this.controller.createUsers(req,res))
+        this.router.put("/updateUser/:id",(req,res)=>this.controller.updateUsers(req,res))
+        this.router.delete("/deleteUser/:id",(req,res)=>this.controller.deleteUsers(req,res))
     }
 }
